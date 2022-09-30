02711cb50c06dae175538eb4059b5c29a7a1b440
Zerensky <pure.evil97@mail.ru>
2022-09-30 12:31:27 +0600

    дз 1

---
+++ HW1.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# Пример:
+# - 6782 -> 23
+# - 0,56 -> 11
+
+# num = float(input('Enter any real number: '))
+
+# num_int = int(str(num).replace('.', ''))
+# sum = 0
+
+# while num_int > 0:
+#     cut_digit = num_int % 10
+#     sum += cut_digit
+#     num_int //= 10
+
+# print(sum)
